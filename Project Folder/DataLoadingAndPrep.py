17b5e74b3a9fbeec07fc41f9e43c6865131f1ad0
Frederik S. Olsen <s204118@student.dtu.dk>
2023-01-07 18:52:35 +0100

    Updated: Dataloader for .npy and .tiff files returns numpy array of image instead of tensor image

--- Project Folder/DataLoadingAndPrep.py
+++ Project Folder/DataLoadingAndPrep.py
@@ -20,18 +20,19 @@ class dataloader(Dataset):
 
         if self.tiff == True:
             image = np.array(Image.open(img_loc)) # Loading image with PIL
-            image = image.astype("int16") # Converting from uint16 to int16
-            transform = transforms.Compose([transforms.ToTensor()]) # Function that transform numpy array into Pytorch tensor
-            tensor_image = transform(image)
+            np_image = image.astype("float16") # Converting from uint16 to int16
+            # transform = transforms.Compose([transforms.ToTensor()]) # Function that transform numpy array into Pytorch tensor
+            # tensor_image = transform(np_image)
         else:
             image = np.load(img_loc)  # Loading image with PIL
-            convert_image = image.astype("int16")  # Converting from uint16 to int16
-            tensor_trans = torch.from_numpy(convert_image)  # Transforming numpy image to Pytorch tensor
-            tensor_image = tensor_trans.permute(2, 0, 1)  # changing from shape [68, 68, 3] to [3, 68, 68] as torch expect
+            convert_image = image.astype("float16")  # Converting from uint16 to int16
+            np_image = convert_image.ravel()
+            # tensor_trans = torch.from_numpy(np_image)  # Transforming numpy image to Pytorch tensor
+            # tensor_image = tensor_trans.permute(2, 0, 1)  # changing from shape [68, 68, 3] to [3, 68, 68] as torch expect
 
         #Creating train, test and validation
 
-        return tensor_image
+        return np_image
 
 #
 dataset= dataloader(path, tiff=True) # takes either .npy or .tiff files
