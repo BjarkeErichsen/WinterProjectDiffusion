0536a13073adf9f19e64e4b65d4364e999a75416
BjarkeErichsen <97966011+BjarkeErichsen@users.noreply.github.com>
2023-01-15 17:58:29 +0100

    Ændrede

--- Project Folder/DDGMReshapedDigitsWandb.py
+++ Project Folder/DDGMReshapedDigitsWandb.py
@@ -11,70 +11,125 @@ import torchvision.transforms as tt
 from DataLoadingAndPrep import Digits
 from FredeDataLoader import DataImage
 from datetime import datetime
-import wandb
 
-#input eksperiment type
-type_of_eksperiment = dict(using_conv = False, Using_image_dataset = False, run_sweep = False)
+# input eksperiment type
+type_of_eksperiment = dict(using_conv=True, Using_image_dataset=False, reshape_Input=True)
 using_conv = type_of_eksperiment['using_conv']
 Using_image_dataset = type_of_eksperiment['Using_image_dataset']
-run_sweep = type_of_eksperiment['run_sweep']
-
-#normal hyperparams
+reshape_Input = type_of_eksperiment["reshape_Input"]
+# normal hyperparams
 PI = torch.from_numpy(np.asarray(np.pi))
 EPS = 1.e-7
-D = 64   # input dimension
+D = 64  # input dimension
 M = 256  # the number of neurons in scale (s) and translation (t) nets
-T = 5  #number of steps
+T = 5  # number of steps
 beta = 0.8
-lr = 1e-4 #1e-4 # learning rate
-num_epochs = 10 # max. number of epochs
-max_patience = 10 # an early stopping is used, if training doesn't improve for longer than 20 epochs, it is stopped
-
-#tilføjede hyperparametre
-if Using_image_dataset:
-    D = 13872
-conv_channels = 8
+lr = 1e-4  # 1e-4 # learning rate
+num_epochs = 500  # max. number of epochs
+max_patience = 40  # an early stopping is used, if training doesn't improve for longer than 20 epochs, it is stopped
 batch_size = 32
 
-#networks:
+# tilføjede hyperparametre
+if Using_image_dataset:
+    D = 13872
+
+# networks:
 if using_conv:
-    p_dnns = [nn.Sequential(nn.Conv1d(in_channels=1, out_channels=conv_channels, kernel_size=3, padding = 1), nn.ReLU(),
-                            nn.Conv1d(in_channels=conv_channels, out_channels=conv_channels, kernel_size=3, padding = 1), nn.ReLU(),
+    conv_channels = 8
+    k_size = 3  # must be 3, 5, 7 etc i e. not even numbers the first conv layer has K size + 2
+    """
+    p_dnns = [nn.Sequential(nn.Conv2d(in_channels=1, out_channels=conv_channels, kernel_size=k_size, padding = int((k_size-1)/2)), nn.ReLU(),
+                            nn.Conv2d(in_channels=conv_channels, out_channels=conv_channels, kernel_size=k_size,padding = int((k_size-1)/2)), nn.ReLU(),
+                            nn.Conv2d(in_channels=conv_channels, out_channels=conv_channels, kernel_size=k_size,padding=int((k_size-1)/2)), nn.ReLU(),
                             nn.Flatten(),
                             nn.Linear(512, M), nn.LeakyReLU(),
                             nn.Linear(M, M), nn.LeakyReLU(),
                             nn.Linear(M, M), nn.LeakyReLU(),
                             nn.Linear(M, 2 * D)) for _ in range(T-1)]
-    decoder_net = nn.Sequential(nn.Conv1d(in_channels=1, out_channels=conv_channels, kernel_size=3, padding = 1), nn.ReLU(),
-                                nn.Conv1d(in_channels=conv_channels, out_channels=conv_channels, kernel_size=3, padding = 1), nn.ReLU(),
+    decoder_net = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=conv_channels, kernel_size=k_size, padding = int((k_size-1)/2)), nn.ReLU(),
+                                nn.Conv2d(in_channels=conv_channels, out_channels=conv_channels, kernel_size=k_size,padding = int((k_size-1)/2)), nn.ReLU(),
+                                nn.Conv2d(in_channels=conv_channels, out_channels=conv_channels, kernel_size=k_size,padding=int((k_size-1)/2)), nn.ReLU(),
                                 nn.Flatten(),
                                 nn.Linear(512, M*2), nn.LeakyReLU(),
                                 nn.Linear(M*2, M*2), nn.LeakyReLU(),
                                 nn.Linear(M*2, M*2), nn.LeakyReLU(),
                                 nn.Linear(M*2, D), nn.Tanh())
-else:
+    """
+    p_dnns = [nn.Sequential(
+        nn.Conv2d(1, 32, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
 
+        nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
+
+        nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
+
+        nn.Flatten(),
+        nn.Linear(256, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 512),
+        nn.ReLU(),
+        nn.Linear(512, 2 * D)) for _ in range(T - 1)]
+
+    decoder_net = nn.Sequential(
+        nn.Conv2d(1, 32, kernel_size=3, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
+
+        nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
+
+        nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
+
+        nn.Flatten(),
+        nn.Linear(256, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, 512),
+        nn.ReLU(),
+        nn.Linear(512, D))
+else:
     p_dnns = [nn.Sequential(nn.Linear(D, M), nn.LeakyReLU(),
                             nn.Linear(M, M), nn.LeakyReLU(),
                             nn.Linear(M, M), nn.LeakyReLU(),
-                            nn.Linear(M, 2 * D)) for _ in range(T-1)]
-    decoder_net = nn.Sequential(nn.Linear(D, M*2), nn.LeakyReLU(),
-                                nn.Linear(M*2, M*2), nn.LeakyReLU(),
-                                nn.Linear(M*2, M*2), nn.LeakyReLU(),
-                                nn.Linear(M*2, D), nn.Tanh())
+                            nn.Linear(M, 2 * D)) for _ in range(T - 1)]
+    decoder_net = nn.Sequential(nn.Linear(D, M * 2), nn.LeakyReLU(),
+                                nn.Linear(M * 2, M * 2), nn.LeakyReLU(),
+                                nn.Linear(M * 2, M * 2), nn.LeakyReLU(),
+                                nn.Linear(M * 2, D), nn.Tanh())
 
 
-#helper functions
+# helper functions
 def log_normal_diag(x, mu, log_var, reduction=None, dim=None):
-    log_p = -0.5 * torch.log(2. * PI) - 0.5 * log_var - 0.5 * torch.exp(-log_var) * (x - mu)**2.
+    log_p = -0.5 * torch.log(2. * PI) - 0.5 * log_var - 0.5 * torch.exp(-log_var) * (x - mu) ** 2.
     if reduction == 'avg':
         return torch.mean(log_p, dim)
     elif reduction == 'sum':
         return torch.sum(log_p, dim)
     else:
         return log_p
+
+
 def log_standard_normal(x, reduction=None, dim=None):
-    log_p = -0.5 * torch.log(2. * PI) - 0.5 * x**2.
+    log_p = -0.5 * torch.log(2. * PI) - 0.5 * x ** 2.
     if reduction == 'avg':
         return torch.mean(log_p, dim)
     elif reduction == 'sum':
@@ -102,7 +157,7 @@ class DDGM(nn.Module):
 
     @staticmethod
     def reparameterization(mu, log_var):
-        std = torch.exp(0.5*log_var)
+        std = torch.exp(0.5 * log_var)
         eps = torch.randn_like(std)
         return mu + std * eps
 
@@ -115,30 +170,38 @@ class DDGM(nn.Module):
         zs = [self.reparameterization_gaussian_diffusion(x, 0)]
 
         for i in range(1, self.T):
-            zs.append(self.reparameterization_gaussian_diffusion(zs[-1], i))
+            zs.append(self.reparameterization_gaussian_diffusion(zs[-1],
+                                                                 i))  # bemærk zs[-1] altså vi adder støj til sidste iteration
 
         # =====
         # backward diffusion
         mus = []
         log_vars = []
 
-        for i in range(len(self.p_dnns) - 1, -1, -1):
-            h = self.p_dnns[i](zs[i+1])
-            mu_i, log_var_i = torch.chunk(h, 2, dim=1)
+        for i in range(len(self.p_dnns) - 1, -1, -1):  # bemærk vi går i den negative retning 3, 2, 1, 0
+            h = self.p_dnns[i](zs[i + 1])
+            mu_i, log_var_i = torch.chunk(h, 2, dim=-1)
             mus.append(mu_i)
             log_vars.append(log_var_i)
 
         mu_x = self.decoder_net(zs[0])
+        if using_conv:
+            _shape = x.shape
+            x = x.reshape((_shape[0], _shape[1], _shape[2] * _shape[3]))
+            for i in range(len(zs)):
+                zs[i] = zs[i].reshape((_shape[0], _shape[1], _shape[2] * _shape[3]))
 
         # =====ELBO
-        # RE
+        # RE        #loss for reconstruction of final layer p(X|Z)
         RE = log_standard_normal(x - mu_x).sum(-1)
 
-        # KL
-        KL = (log_normal_diag(zs[-1], torch.sqrt(1. - self.beta) * zs[-1], torch.log(self.beta)) - log_standard_normal(zs[-1])).sum(-1)
+        # KL        #the KL divergence of each layer ie. E[log(q(Z) / p(Z))]
+        KL = (log_normal_diag(zs[-1], torch.sqrt(1. - self.beta) * zs[-1], torch.log(self.beta)) - log_standard_normal(
+            zs[-1])).sum(-1)
 
         for i in range(len(mus)):
-            KL_i = (log_normal_diag(zs[i], torch.sqrt(1. - self.beta) * zs[i], torch.log(self.beta)) - log_normal_diag(zs[i], mus[i], log_vars[i])).sum(-1)
+            KL_i = (log_normal_diag(zs[i], torch.sqrt(1. - self.beta) * zs[i], torch.log(self.beta)) - log_normal_diag(
+                zs[i], mus[i], log_vars[i])).sum(-1)
 
             KL = KL + KL_i
         # KL, RE
@@ -154,13 +217,14 @@ class DDGM(nn.Module):
         z = torch.randn([batch_size, self.D])
         if using_conv:
             z = torch.unsqueeze(z, 1)  # Bjarke added this
-
+            z = z.reshape((z.shape[0], 1, 8, 8))
         for i in range(len(self.p_dnns) - 1, -1, -1):
             h = self.p_dnns[i](z)
-            mu_i, log_var_i = torch.chunk(h, 2, dim=1) #splits the tensor into 2
+            mu_i, log_var_i = torch.chunk(h, 2, dim=-1)  # splits the tensor into 2
             z = self.reparameterization(torch.tanh(mu_i), log_var_i)
             if using_conv:
                 z = torch.unsqueeze(z, 1)  # Bjarke added this
+                z = z.reshape((z.shape[0], 1, 8, 8))
 
         mu_x = self.decoder_net(z)
 
@@ -174,6 +238,7 @@ class DDGM(nn.Module):
 
         return zs[-1]
 
+
 def evaluation(test_loader, name=None, model_best=None, epoch=None):
     # EVALUATION
     if model_best is None:
@@ -184,7 +249,7 @@ def evaluation(test_loader, name=None, model_best=None, epoch=None):
     N = 0.
     for indx_batch, test_batch in enumerate(test_loader):
         if using_conv:
-            test_batch = torch.unsqueeze(test_batch, 1) #Bjarke added this
+            test_batch = torch.unsqueeze(test_batch, 1)  # Bjarke added this
 
         loss_t = model_best.forward(test_batch, reduction='sum')
         loss = loss + loss_t.item()
@@ -198,13 +263,12 @@ def evaluation(test_loader, name=None, model_best=None, epoch=None):
 
     return loss
 
+
 def training(name, max_patience, num_epochs, model, optimizer, training_loader, val_loader):
     nll_val = []
     best_nll = 1000.
     patience = 0
 
-
-    wandb.watch(model, log="all", log_freq = 1000)
     # Main loop
     for e in range(num_epochs):
         # TRAINING
@@ -221,9 +285,8 @@ def training(name, max_patience, num_epochs, model, optimizer, training_loader, 
         # Validation
         loss_val = evaluation(val_loader, model_best=model, epoch=e)
         nll_val.append(loss_val)  # save for plotting
-        wandb.log({'validation nll':loss_val }, step=e)
+
         if e == 0:
-            torch.save(model, name + '.model')
             print('started')
             best_nll = loss_val
         else:
@@ -244,7 +307,8 @@ def training(name, max_patience, num_epochs, model, optimizer, training_loader, 
 
     return nll_val
 
-#sample a real image
+
+# sample a real image
 def samples_real(name, test_loader):
     # REAL-------
     num_x = 4
@@ -257,9 +321,11 @@ def samples_real(name, test_loader):
         ax.imshow(plottable_image, cmap='gray')
         ax.axis('off')
 
-    plt.savefig(name+'_real_images.pdf', bbox_inches='tight')
+    plt.savefig(name + '_real_images.pdf', bbox_inches='tight')
     plt.close()
-#sample from backward diffusion that are moved to results
+
+
+# sample from backward diffusion that are moved to results
 def samples_generated(name, data_loader, extra_name=''):
     # GENERATIONS-------
     model_best = torch.load(name + '.model')
@@ -278,7 +344,9 @@ def samples_generated(name, data_loader, extra_name=''):
 
     plt.savefig(name + '_generated_images' + extra_name + '.pdf', bbox_inches='tight')
     plt.close()
-#sample forward diffusion
+
+
+# sample forward diffusion
 def samples_diffusion(name, data_loader, extra_name=''):
     x = next(iter(data_loader))
 
@@ -299,7 +367,9 @@ def samples_diffusion(name, data_loader, extra_name=''):
 
     plt.savefig(name + '_generated_diffusion' + extra_name + '.pdf', bbox_inches='tight')
     plt.close()
-#plot of nll = negative log likelihood, we of course want to minimize negative log likelihood
+
+
+# plot of nll = negative log likelihood, we of course want to minimize negative log likelihood
 def plot_curve(name, nll_val):
     plt.plot(np.arange(len(nll_val)), nll_val, linewidth='3')
     plt.xlabel('epochs')
@@ -307,7 +377,8 @@ def plot_curve(name, nll_val):
     plt.savefig(name + '_nll_val_curve.pdf', bbox_inches='tight')
     plt.close()
 
-def test(model, test_loader, nll_val):
+
+def final_test_and_saving(model, test_loader, nll_val):
     # Final evaluation
     test_loss = evaluation(name=result_dir + name, test_loader=test_loader)
     f = open(result_dir + name + '_test_loss.txt', "w")
@@ -321,93 +392,45 @@ def test(model, test_loader, nll_val):
     samples_generated(result_dir + name, test_loader, extra_name='FINAL')
     samples_diffusion(result_dir + name, test_loader, extra_name='DIFFUSION')
 
-
-    #the following is just a convoluted way of saving the model
+    # the following is just a convoluted way of saving the model
     model.eval()
     with torch.no_grad():
         for indx_batch, test_batch in enumerate(test_loader):
             if using_conv:
-                test_batch = torch.unsqueeze(test_batch, 1) #Bjarke added this
+                test_batch = torch.unsqueeze(test_batch, 1)  # Bjarke added this
             dummy_input = test_batch
             outputs = model(test_batch)
             break
-    torch.onnx.export(model, dummy_input, "model.onnx", input_names = "noice", output_names = "digit")
-    wandb.save("model.onnx")
-
-
-
-
-
-def make(config):
-    T = config.T
-    M = config.M
-    model = DDGM(p_dnns, decoder_net, beta=beta, T=T, D=D)
-    optimizer = torch.optim.Adamax([p for p in model.parameters() if p.requires_grad == True], lr=lr)
-
-    return model, optimizer
-def model_pipeline(hyperparams):
-
-    with wandb.init(project="pytorch-demo", config = hyperparams):
-        config = wandb.config
-        # Eventually, we initialize the full model
-        model, optimizer = make(config)
-        print(model)
-
-        # Training procedure
-        nll_val = training(name=result_dir + name, max_patience=max_patience, num_epochs=num_epochs, model=model, optimizer=optimizer,
-                               training_loader=training_loader, val_loader=val_loader)
-
-        #test(model, test_loader, nll_val)
-
 
 
 if __name__ == "__main__":
 
     transforms = tt.Lambda(lambda x: 2. * (x / 17.) - 1.)
+
     if Using_image_dataset:
         train_data = DataImage(mode='train')
         val_data = DataImage(mode='val')
         test_data = DataImage(mode='test')
     else:
-        train_data = Digits(mode='train', transforms=transforms)
-        val_data = Digits(mode='val', transforms=transforms)
-        test_data = Digits(mode='test', transforms=transforms)
+        train_data = Digits(mode='train', transforms=transforms, reshape=reshape_Input)
+        val_data = Digits(mode='val', transforms=transforms, reshape=reshape_Input)
+        test_data = Digits(mode='test', transforms=transforms, reshape=reshape_Input)
 
     training_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
-    name = 'Diffusion' + '_' + "T_" + str(T) + '_' + "beta_" + str(beta) + '_' + 'M_' + str(M)
+    name = 'Diffusion' + '_' + "Conv_" + str(using_conv) + "_T_" + str(T) + '_' + "beta_" + str(
+        beta) + '_' + 'M_' + str(M)
     result_dir = 'Results/' + name + '/'
     if not (os.path.exists(result_dir)):
         os.makedirs(result_dir)
-
-
-
-    if run_sweep:
-        sweep_config = {
-            'method': 'grid',
-            'name': 'sweep',
-            'metric': {
-                'name': 'nll',
-                'goal': 'minimize'},
-            'parameters': {
-                'T': {'values': [3, 6, 15]},
-                'M': {'values': [256, 64]}
-            }
-        }
-            #.init(project="Diffusion", config = sweep_config)
-            #sweep_id = wandb.sweep(sweep=sweep_config, project="Diffusion")
-            #wandb.agent(sweep_id, function=model_pipeline, count = 3)
-    else:
-        config = dict(T = 6, M = 65)
-        model_pipeline(config)
 
     model = DDGM(p_dnns, decoder_net, beta=beta, T=T, D=D)
     optimizer = torch.optim.Adamax([p for p in model.parameters() if p.requires_grad == True], lr=lr)
     nll_val = training(name=result_dir + name, max_patience=max_patience, num_epochs=num_epochs, model=model,
                        optimizer=optimizer,
                        training_loader=training_loader, val_loader=val_loader)
-    test(model, test_loader, nll_val)
 
+    final_test_and_saving(model, test_loader, nll_val)
 
